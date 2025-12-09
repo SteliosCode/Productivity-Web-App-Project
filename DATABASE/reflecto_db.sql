007f694195e225e4f4f24e99b82bb90b74cf94f1
Stelios Giatrakos <stelios.giatrakos858@gmail.com>
2025-12-09 20:50:06 +0200

    added the github folder

---
+++ DATABASE/reflecto_db.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2025 at 08:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reflecto_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ai_analysis`
+--
+
+CREATE TABLE `ai_analysis` (
+  `ai_id` int(11) NOT NULL,
+  `sentiment` varchar(20) DEFAULT NULL,
+  `confidence_score` float DEFAULT NULL,
+  `keywords` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `solution_suggestion` text DEFAULT NULL,
+  `sr_comments_id` int(11) DEFAULT NULL,
+  `log_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `self_reflection_comments`
+--
+
+CREATE TABLE `self_reflection_comments` (
+  `sr_comments_id` int(11) NOT NULL,
+  `text` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `task_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_logs`
+--
+
+CREATE TABLE `task_logs` (
+  `log_id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `task_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ai_analysis`
+--
+ALTER TABLE `ai_analysis`
+  ADD PRIMARY KEY (`ai_id`),
+  ADD KEY `sr_comments_id` (`sr_comments_id`),
+  ADD KEY `log_id` (`log_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `self_reflection_comments`
+--
+ALTER TABLE `self_reflection_comments`
+  ADD PRIMARY KEY (`sr_comments_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `task_logs`
+--
+ALTER TABLE `task_logs`
+  ADD PRIMARY KEY (`log_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ai_analysis`
+--
+ALTER TABLE `ai_analysis`
+  ADD CONSTRAINT `ai_analysis_ibfk_1` FOREIGN KEY (`sr_comments_id`) REFERENCES `self_reflection_comments` (`sr_comments_id`),
+  ADD CONSTRAINT `ai_analysis_ibfk_2` FOREIGN KEY (`log_id`) REFERENCES `task_logs` (`log_id`);
+
+--
+-- Constraints for table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `self_reflection_comments`
+--
+ALTER TABLE `self_reflection_comments`
+  ADD CONSTRAINT `self_reflection_comments_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`);
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
+  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `task_logs`
+--
+ALTER TABLE `task_logs`
+  ADD CONSTRAINT `task_logs_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
